e1ab790a585a3d5f579091458aee3180c1260ba1
Sil van Diepen <me@sil.mt>
2022-02-10 14:41:12 +0100

    [refactor] show copied

--- src/components/IconDetail/IconDetail.ts
+++ src/components/IconDetail/IconDetail.ts
@@ -1,9 +1,11 @@
-import { defineComponent, PropType, computed } from "vue";
+import { defineComponent, PropType, computed, ref } from "vue";
 import { Style } from "@sil/tools";
+import { SnackBarComponent } from "../SnackBar";
 import { Icon } from "../../data/icons/icons.model";
 import { useIcons } from "../../composables/useIcons";
 
 export default defineComponent({
+  components: { SnackBar: SnackBarComponent },
   props: {
     icon: {
       type: Object as PropType<Icon>,
@@ -19,6 +21,10 @@ export default defineComponent({
       navigator.clipboard.writeText(code).then(
         () => {
           console.log(`copied ${code}`);
+          showCopy.value = `Copied ${code}`;
+          setTimeout(() => {
+            showCopy.value = "";
+          }, 1000);
         },
         (err) => {
           console.error(`${code} couldn't be copied, because; ${err}`);
@@ -43,6 +49,8 @@ export default defineComponent({
       }
     });
 
+    const showCopy = ref("");
+
     const toggleFavorite = () => {
       toggleFavoriteIcon(props.icon?.unicode);
     };
@@ -52,6 +60,7 @@ export default defineComponent({
       isFavorite,
       details,
       style,
+      showCopy,
     };
   },
 });
